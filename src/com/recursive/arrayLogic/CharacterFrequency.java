d510e99b020b254047cef8551eae747caa3ef2ac
Prajju1234567 <prajwalprajju0703@gmail.com>
2026-02-25 16:26:04 +0530

    Commited Program

---
+++ src/com/recursive/arrayLogic/CharacterFrequency.java
@@ -0,0 +1,17 @@
+package com.recursive.arrayLogic;
+
+import java.util.TreeMap;
+
+public class CharacterFrequency {
+	public static void main(String[] args) {
+		String str = "Hello";
+		getCharacterFrequency(str);
+	}
+
+	public  static void getCharacterFrequency(String str) {
+			TreeMap<Character, Integer> map = new TreeMap<Character, Integer>();
+			char[] c = str.toCharArray();
+			
+	}
+
+}
